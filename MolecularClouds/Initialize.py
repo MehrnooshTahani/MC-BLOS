3cec15a6f6e54e837334521f7e6a35ea36a5c609
John-Ming-Ngo <john.ming.ngo@gmail.com>
2022-08-11 11:34:39 -0600

    Changed the setup scripts to be in alphabetical order.

--- MolecularClouds/Initialize.py
+++ MolecularClouds/Initialize.py
@@ -2,9 +2,9 @@
 Runs all the pre-analysis steps which need to be run only once.
 '''
 import subprocess
-scripts = ["00InstallPackages.py",
-           "00MakeConfig.py",
-           "00DownloadExampleData.py"]
+scripts = ["00aInstallPackages.py",
+           "00bMakeConfig.py",
+           "00cDownloadExampleData.py"]
 for script in scripts:
     print("===========================================================")
     print("Script: {}".format(script))
