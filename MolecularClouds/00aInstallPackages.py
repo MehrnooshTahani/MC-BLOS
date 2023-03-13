19bee40fde88a78981fde515964b69614017d509
John-Ming-Ngo <john.ming.ngo@gmail.com>
2023-03-13 16:43:59 -0600

    Avoiding package versioning issues by specifying package versions

--- MolecularClouds/00aInstallPackages.py
+++ MolecularClouds/00aInstallPackages.py
@@ -5,17 +5,18 @@ import sys
 import subprocess
 
 #Install packages:
-packages = ['numpy',
-            'scipy',
-            'astropy',
-            'pandas',
-            'matplotlib',
-            'adjusttext',
-            'requests',
-            'sklearn']
-commands = [[sys.executable, '-m', 'pip', 'install', '--upgrade', package] for package in packages]
+packages = ['numpy==1.19.5',
+            'scipy==1.5.4',
+            'astropy==4.1',
+            'pandas==1.1.5',
+            'matplotlib==3.3.4',
+            'adjusttext==0.7.3',
+            'requests==2.27.1',
+            'sklearn==0.0']
+commands = [[sys.executable, '-m', 'pip', 'install', '--force-reinstall', package] for package in packages]
 subprocess.run([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'], shell=False)
 #Install all the packages.
 for command in commands:
     subprocess.run(command, shell=False)
+
 
