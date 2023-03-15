c5eba18783a67c0f48559c24fba6c9d3cf38f1c2
John-Ming-Ngo <john.ming.ngo@gmail.com>
2023-03-15 17:56:47 -0600

    Main package that needs to be version specified is adjustText. The rest is optional. Pandas 1.1.5 testing needed in 00InstallPackages.

--- MolecularClouds/00aInstallPackages.py
+++ MolecularClouds/00aInstallPackages.py
@@ -5,16 +5,27 @@ import sys
 import subprocess
 
 #Install packages:
-packages = ['numpy==1.19.5',
-            'scipy==1.5.4',
-            'astropy==4.1',
-            'pandas==1.1.5',
-            'matplotlib==3.3.4',
+'''packages = ['numpy<=1.24.2',
+            'scipy<=1.10.1',
+            'astropy<=5.2.1',
+            'pandas<=1.5.3',
+            'matplotlib<=3.7.1',
             'adjusttext==0.7.3',
-            'requests==2.27.1',
+            'requests<=2.28.2',
+            'sklearn==0.0']'''
+packages = ['numpy',
+            'scipy',
+            'astropy',
+            'pandas<=1.1.5',
+            'matplotlib',
+            'adjusttext==0.7.3',
+            'requests',
             'sklearn==0.0']
 commands = [[sys.executable, '-m', 'pip', 'install', '--force-reinstall', package] for package in packages]
 subprocess.run([sys.executable, '-m', 'pip', 'install', '--upgrade', 'pip'], shell=False)
+#subprocess.run([sys.executable, '-m', 'pip', 'install', '--upgrade', 'setuptools'], shell=False)
+#subprocess.run([sys.executable, '-m', 'pip', 'install', '--upgrade', 'wheel'], shell=False)
+#subprocess.run([sys.executable, '-m', 'pip', 'install', '--upgrade', 'cython'], shell=False)
 #Install all the packages.
 for command in commands:
     subprocess.run(command, shell=False)
